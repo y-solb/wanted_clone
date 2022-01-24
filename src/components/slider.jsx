e2f9e492685d00b06d40f2a587a91a2387dffe37
y-solb <ysolb99@gmail.com>
2022-01-24 21:45:08 +0900

    refactor: Refactoring slider component

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,72 +1,63 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import "../App.css";
-import InfoBox from "./infoBox";
 
 const Slider = ({ sliderData }) => {
-  const [curIndex, setCurIndex] = useState(2);
-  const [offset, setOffset] = useState(-2168);
-  const [speed, setSpeed] = useState(500);
+  const IMG_WIDTH = 1084;
   const length = sliderData.length;
-  const btnRef = useRef();
 
-  useEffect(() => {
-    const timer = setInterval(nextSlide, 5000);
-    return () => clearInterval(timer);
-  }, []);
+  const sliderRef = useRef(null);
+  const [curIndex, setCurIndex] = useState(2);
+  const [speed, setSpeed] = useState(500);
 
   const prevSlide = () => {
-    if (curIndex >= 0) {
+    if (curIndex > 0) {
       setSpeed(500);
-      setOffset((offset) => parseInt(offset) + 1084);
       setCurIndex((curIndex) => curIndex - 1);
     }
     if (curIndex === 2) {
       setTimeout(() => {
         setSpeed(0);
-        setOffset(-10840);
         setCurIndex(10);
       }, 500);
     }
   };
 
-  const nextSlide = () => {
-    if (curIndex <= length - 1) {
+  const nextSlide = useCallback(() => {
+    if (curIndex < length - 2) {
       setSpeed(500);
-      setOffset((offset) => parseInt(offset) - 1084);
       setCurIndex((curIndex) => curIndex + 1);
     }
-    if (curIndex === 9) {
+    if (curIndex === 10) {
       setTimeout(() => {
         setSpeed(0);
-        setOffset(-1084);
-        setCurIndex(1);
+        setCurIndex(2);
       }, 500);
     }
-  };
+  }, [curIndex, length]);
+
+  useEffect(() => {
+    sliderRef.current.style.transform = `translate3d( -${
+      curIndex * IMG_WIDTH - 214
+    }px, 0, 0)`;
+    sliderRef.current.style.transition = `${speed}ms ease`;
+    const timer = setInterval(nextSlide, 5000);
+    return () => clearInterval(timer);
+  }, [curIndex, speed, nextSlide]);
 
   return (
-    <div className="slider">
-      <div className="slider__container">
-        <div
-          className="slider__track"
-          style={{
-            transform: `translate3d( ${offset}px, 0, 0)`,
-            transition: `${speed}ms ease 0s`,
-          }}
-        >
-          {sliderData.map((slider, index) => {
-            return (
-              <div className="slider__box" key={index}>
-                <img
-                  className="slider__img"
-                  src={slider.img}
-                  alt="slider_image"
-                />
-                <InfoBox />
-              </div>
-            );
-          })}
-        </div>
+    <div className="slider__container">
+      <div className="slider" ref={sliderRef}>
+        {sliderData.map((slider, index) => {
+          return (
+            <div className="slider__box" key={index}>
+              <img
+                className="slider__img"
+                src={slider.img}
+                alt="slider__image"
+              />
+            </div>
+          );
+        })}
       </div>
       <button className="left__btn" onClick={prevSlide}>
         <span className="arrow">
@@ -75,7 +66,7 @@ const Slider = ({ sliderData }) => {
           </svg>
         </span>
       </button>
-      <button className="right__btn" onClick={nextSlide} ref={btnRef}>
+      <button className="right__btn" onClick={nextSlide}>
         <span className="arrow">
           <svg className="arrow__icon" viewBox="0 0 18 18">
             <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>

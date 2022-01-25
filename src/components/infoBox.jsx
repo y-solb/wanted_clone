af51e01985ae9b6cc3524e777ccd98a6e93d5c8f
y-solb <ysolb99@gmail.com>
2022-01-25 11:09:50 +0900

    refactor: Refactoring info component

--- src/components/infoBox.jsx
+++ src/components/infoBox.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "../App.css";
 
-const InfoBox = (props) => (
-  <div className="info__box">
-    <h2>성장하는 개발자가 되려면?</h2>
-    <h3>OOO 검색하지 말 것!</h3>
+const InfoBox = ({ slider, displayType }) => (
+  <div className={`info__box ${displayType}`}>
+    <p className="info__title">{slider.title}</p>
+    <p className="info__sub">{slider.sub}</p>
     <hr class="info__divider"></hr>
-    <span>
+    <span className="info__link">
       바로가기
-      <svg>
+      <svg className="link__btn" viewBox="0 0 18 18">
         <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
       </svg>
     </span>
